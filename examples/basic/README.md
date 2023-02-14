12769a0f30b5ad4c101eec37070a742935b199e2
Aashiq-Jacob <aashiq.jacob@ibm.com>
2023-02-14 14:43:50 +0530

    feat: initial commit

---
+++ examples/basic/README.md
@@ -0,0 +1,3 @@
+# Deploy basic observability agents
+
+An end to end example using the module's default variable values.
--- version.tf
+++ version.tf
@@ -1,11 +1,15 @@
 terraform {
-  required_version = ">= 1.0.0"
-  # Add any required providers below and uncomment
-  #  required_providers {
-  #    ibm = {
-  #      source = "IBM-Cloud/ibm"
-  #      # Use "greater than or equal to" range in modules
-  #      version = ">= 1.45.0"
-  #    }
-  #  }
+  # module uses nullable feature which is only available in versions >= 1.1.0
+  required_version = ">= 1.1.0"
+  required_providers {
+    ibm = {
+      # Use "greater than or equal to" range in modules
+      source  = "ibm-cloud/ibm"
+      version = ">= 1.49.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.8.0"
+    }
+  }
 }
