6c9f013e11bf1a5e68e236a3a0ce012812373215
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2023-02-17 11:25:42 +0000

    Update provider.tf

--- examples/basic/provider.tf
+++ examples/basic/provider.tf
@@ -3,6 +3,22 @@ provider "ibm" {
   region           = var.region
 }
 
+locals {
+  at_endpoint = "https://api.${var.region}.logging.cloud.ibm.com"
+}
+
+provider "logdna" {
+  alias      = "at"
+  servicekey = module.test_observability_instance_creation.activity_tracker_resource_key != null ? module.test_observability_instance_creation.activity_tracker_resource_key : ""
+  url        = local.at_endpoint
+}
+
+provider "logdna" {
+  alias      = "ld"
+  servicekey = module.test_observability_instance_creation.logdna_resource_key != null ? module.test_observability_instance_creation.logdna_resource_key : ""
+  url        = local.at_endpoint
+}
+
 provider "kubernetes" {
   host  = data.ibm_container_cluster_config.cluster_config.host
   token = data.ibm_container_cluster_config.cluster_config.token
