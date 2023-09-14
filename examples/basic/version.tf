cbd000e21c5d2972bcdeb528fef39805b3046fdc
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2023-09-14 21:22:11 +0100

    Update version.tf

--- examples/basic/version.tf
+++ examples/basic/version.tf
@@ -3,26 +3,22 @@ terraform {
   required_version = ">= 1.1.0"
 
   required_providers {
-    # Pin to the lowest provider version of the range defined in the main module to ensure lowest version still works
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = "1.49.0"
+      version = ">= 1.49.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "2.8.0"
+      version = ">= 2.8.0"
     }
-    # The kubernetes provider is not actually required by the module itself, just this example, so OK to use ">=" here instead of locking into a version
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = ">= 2.16.1"
-    }
-    # The time provider is not actually required by the module itself, just this example, so OK to use ">=" here instead of locking into a version
+    }    
     time = {
       source  = "hashicorp/time"
       version = ">= 0.9.1"
     }
-    # The logdna provider is not actually required by the module itself, just this example, so OK to use ">=" here instead of locking into a version
     logdna = {
       source  = "logdna/logdna"
       version = ">= 1.14.2"
