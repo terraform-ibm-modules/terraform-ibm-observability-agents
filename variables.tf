51e1325a3d911e6dd93548bfb5a03875d5e1f1be
Valerio Bontempi <valerio.bontempi@it.ibm.com>
2023-11-23 18:10:31 +0100

    feat: added support for LogDNA metadata filtering (#226)

--- variables.tf
+++ variables.tf
@@ -59,6 +59,21 @@ variable "logdna_add_cluster_name" {
   default     = true
 }
 
+# LogDNA agent custom settings to setup Kubernetes metadata logs filtering
+# Ref https://github.com/logdna/logdna-agent-v2/blob/3.8/docs/KUBERNETES.md#configuration-for-kubernetes-metadata-filtering
+
+variable "logdna_agent_custom_line_inclusion" {
+  description = "LogDNA agent custom configuration for line inclusion setting LOGDNA_K8S_METADATA_LINE_INCLUSION."
+  type        = string
+  default     = null
+}
+
+variable "logdna_agent_custom_line_exclusion" {
+  description = "LogDNA agent custom configuration for line exclusion setting LOGDNA_K8S_METADATA_LINE_EXCLUSION."
+  type        = string
+  default     = null
+}
+
 variable "sysdig_enabled" {
   type        = bool
   description = "Deploy IBM Cloud Monitoring agent"
