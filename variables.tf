4f65a709318e72616e871a2ece43f916a6c7b712
Daniel Butler <dannyeb@gmail.com>
2023-12-04 10:12:13 +0000

    remove image input vars

--- variables.tf
+++ variables.tf
@@ -31,14 +31,6 @@ variable "logdna_resource_group_id" {
   default     = null
 }
 
-variable "logdna_agent_version" {
-  type        = string
-  description = "Version of the agent to deploy. To lookup version run: `ibmcloud cr images --restrict ext/logdna-agent`. If null, the default value is used."
-  # renovate: datasource=docker depName=icr.io/ext/logdna-agent versioning=regex:^(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)-(?<build>\d{8}).\w+?$
-  default  = "3.9.0-20231127.1da2b0706ea2d823"
-  nullable = false
-}
-
 variable "logdna_agent_tags" {
   type        = list(string)
   description = "array of tags to group the host logs pushed by the logdna agent"
@@ -90,14 +82,6 @@ variable "sysdig_resource_group_id" {
   type        = string
   description = "Resource group that the IBM Cloud Monitoring is in. Defaults to Clusters group"
   default     = null
-}
-
-variable "sysdig_agent_version" {
-  type        = string
-  description = "IBM Cloud Monitoring Agent Version. To lookup version run: `ibmcloud cr images --restrict ext/sysdig/agent`. If null, the default value is used."
-  # renovate: datasource=docker depName=icr.io/ext/sysdig/agent
-  default  = "12.18.0"
-  nullable = false
 }
 
 variable "sysdig_access_key" {
