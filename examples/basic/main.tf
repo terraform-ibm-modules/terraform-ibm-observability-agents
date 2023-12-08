5da59bb2deff8853c1babc5c8a054721b655bbb4
mounika-nalla <mounika.nalla@ibm.com>
2023-12-08 13:20:25 -0500

    feat: add public gw

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -40,12 +40,20 @@ resource "ibm_is_vpc" "example_vpc" {
   tags           = var.resource_tags
 }
 
+resource "ibm_is_public_gateway" "public_gateway" {
+  name           = "${var.prefix}-gateway-1"
+  vpc            = ibm_is_vpc.example_vpc.id
+  resource_group = module.resource_group.resource_group_id
+  zone           = "${var.region}-1"
+}
+
 resource "ibm_is_subnet" "testacc_subnet" {
   name                     = "${var.prefix}-subnet"
   vpc                      = ibm_is_vpc.example_vpc.id
   zone                     = "${var.region}-1"
   total_ipv4_address_count = 256
   resource_group           = module.resource_group.resource_group_id
+  public_gateway           = ibm_is_public_gateway.public_gateway.id
 }
 
 resource "ibm_resource_instance" "cos_instance" {
