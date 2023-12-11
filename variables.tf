6bf5869a912d3010a8d11dc914d86450474e9132
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2023-12-11 14:34:42 +0000

    fix(deps): update icr.io/ext/logdna-agent docker tag to v3.9.0-20231211.fe6d770d2b194a59

--- variables.tf
+++ variables.tf
@@ -35,7 +35,7 @@ variable "logdna_agent_version" {
   type        = string
   description = "Version of the agent to deploy. To lookup version run: `ibmcloud cr images --restrict ext/logdna-agent`. If null, the default value is used."
   # renovate: datasource=docker depName=icr.io/ext/logdna-agent versioning=regex:^(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)-(?<build>\d{8}).\w+?$
-  default  = "3.9.0-20231204.f89f0d6c94267329"
+  default  = "3.9.0-20231211.fe6d770d2b194a59"
   nullable = false
 }
 
