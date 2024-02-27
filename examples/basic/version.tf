7734dc4bc8ae4ab7ba8d323a61d1f8aa70501d17
akocbek <106765658+akocbek@users.noreply.github.com>
2024-02-27 19:57:47 +0000

    fix: update required ibm provider version to < 1.7 (#291)
    Co-authored-by: akocbek@ie.ibm.com <akocbek@ie.ibm.com>

--- examples/basic/version.tf
+++ examples/basic/version.tf
@@ -1,6 +1,6 @@
 terraform {
   # module uses nullable feature which is only available in versions >= 1.1.0
-  required_version = ">= 1.1.0, <1.6.0"
+  required_version = ">= 1.1.0, <1.7.0"
 
   required_providers {
     ibm = {
--- version.tf
+++ version.tf
@@ -1,6 +1,6 @@
 terraform {
   # module uses nullable feature which is only available in versions >= 1.1.0
-  required_version = ">= 1.1.0, <1.6.0"
+  required_version = ">= 1.1.0, <1.7.0"
 
   # Each required provider's version should be a flexible range to future proof the module's usage with upcoming minor and patch versions.
   required_providers {
