3d0da8ba27acc272225e316fbe70d2774cb535d9
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-09-15 20:44:04 +0000

    chore(deps): update terraform github.com/terraform-ibm-modules/terraform-ibm-observability-instances to v2.18.0

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -14,7 +14,7 @@ module "resource_group" {
 ##############################################################################
 
 module "observability_instances" {
-  source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-observability-instances?ref=v2.17.0"
+  source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-observability-instances?ref=v2.18.0"
   providers = {
     logdna.at = logdna.at
     logdna.ld = logdna.ld
