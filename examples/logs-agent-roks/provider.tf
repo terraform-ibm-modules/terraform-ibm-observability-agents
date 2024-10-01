ff344537d272428c7d96c45579501d34faf48302
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2024-10-01 13:22:43 +0530

    add cert

--- examples/logs-agent-roks/provider.tf
+++ examples/logs-agent-roks/provider.tf
@@ -5,14 +5,16 @@ provider "ibm" {
 
 provider "helm" {
   kubernetes {
-    host  = data.ibm_container_cluster_config.cluster_config.host
-    token = data.ibm_container_cluster_config.cluster_config.token
+    host                   = data.ibm_container_cluster_config.cluster_config.host
+    token                  = data.ibm_container_cluster_config.cluster_config.token
+    cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
   }
 }
 
 provider "kubernetes" {
-  host  = data.ibm_container_cluster_config.cluster_config.host
-  token = data.ibm_container_cluster_config.cluster_config.token
+  host                   = data.ibm_container_cluster_config.cluster_config.host
+  token                  = data.ibm_container_cluster_config.cluster_config.token
+  cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
 }
 
 locals {
