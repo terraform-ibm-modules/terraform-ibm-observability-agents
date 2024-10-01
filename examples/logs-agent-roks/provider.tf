f18d20ee73f9efc622708934f897de2737160820
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2024-10-01 09:48:27 +0530

    update provider

--- examples/logs-agent-roks/provider.tf
+++ examples/logs-agent-roks/provider.tf
@@ -4,15 +4,21 @@ provider "ibm" {
 }
 
 provider "kubernetes" {
-  host  = data.ibm_container_cluster_config.cluster_config.host
-  token = data.ibm_container_cluster_config.cluster_config.token
+  client_certificate     = data.ibm_container_cluster_config.cluster_config.admin_certificate
+  client_key             = data.ibm_container_cluster_config.cluster_config.admin_key
+  cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
+  host                   = data.ibm_container_cluster_config.cluster_config.host
+  token                  = data.ibm_container_cluster_config.cluster_config.token
 }
+
 
 provider "helm" {
   kubernetes {
+    client_certificate     = data.ibm_container_cluster_config.cluster_config.admin_certificate
+    client_key             = data.ibm_container_cluster_config.cluster_config.admin_key
+    cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
     host                   = data.ibm_container_cluster_config.cluster_config.host
     token                  = data.ibm_container_cluster_config.cluster_config.token
-    cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
   }
 }
 
