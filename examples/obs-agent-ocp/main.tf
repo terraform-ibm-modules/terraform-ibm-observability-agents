fbbcafc8ab01d5c217f6f8e76fd735f0b31a5dde
shemau <shemau@gmail.com>
2024-12-02 11:59:23 +0000

    fix(deps): bump provider 1.70

--- examples/obs-agent-ocp/main.tf
+++ examples/obs-agent-ocp/main.tf
@@ -130,7 +130,7 @@ data "ibm_container_cluster_config" "cluster_config" {
 
 module "observability_instances" {
   source                         = "terraform-ibm-modules/observability-instances/ibm"
-  version                        = "3.0.2"
+  version                        = "3.3.1"
   resource_group_id              = module.resource_group.resource_group_id
   region                         = var.region
   cloud_logs_plan                = "standard"
