99c60b0351661ea4046b8de4ab618f6b5ecb1ee7
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2025-03-05 22:42:13 +0000

    fix: update feedback

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -15,7 +15,7 @@ module "resource_group" {
 ##############################################################################
 
 module "landing_zone" {
-  source                              = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks//module?ref=v6.6.3"
+  source                              = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks//module?ref=v7.3.0"
   region                              = var.region
   prefix                              = var.prefix
   tags                                = var.resource_tags
