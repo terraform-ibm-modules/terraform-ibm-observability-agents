5eacf4b95204540583d1050f78d23bb03da65a6f
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-04-27 23:32:34 +0000

    chore(deps): update terraform-module

--- examples/obs-agent-ocp/main.tf
+++ examples/obs-agent-ocp/main.tf
@@ -104,7 +104,7 @@ locals {
 
 module "ocp_base" {
   source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version              = "3.46.8"
+  version              = "3.46.11"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   tags                 = var.resource_tags
@@ -129,7 +129,7 @@ data "ibm_container_cluster_config" "cluster_config" {
 
 module "observability_instances" {
   source                         = "terraform-ibm-modules/observability-instances/ibm"
-  version                        = "3.5.1"
+  version                        = "3.5.2"
   resource_group_id              = module.resource_group.resource_group_id
   region                         = var.region
   cloud_logs_plan                = "standard"
