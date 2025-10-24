69d6b87c215f11bc2747e7bb8f1efa5508c921a3
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-10-24 20:08:45 +0100

    chore(deps): update terraform terraform-ibm-modules/base-ocp-vpc/ibm to v3.67.1 (#658)

--- examples/obs-agent-ocp/main.tf
+++ examples/obs-agent-ocp/main.tf
@@ -106,7 +106,7 @@ locals {
 
 module "ocp_base" {
   source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version              = "3.66.0"
+  version              = "3.67.1"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   tags                 = var.resource_tags
