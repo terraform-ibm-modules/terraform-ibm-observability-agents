77909f90a880e60b95b8e3c02a34d7fd62bfb24f
Akash Kumar <akash.kumar@ibm.com>
2026-01-09 21:10:27 +0530

    tests: fixed error handling in pr test

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -19,7 +19,11 @@ var sharedInfoSvc *cloudinfo.CloudInfoService
 // TestMain will be run before any parallel tests, used to set up a shared InfoService object to track region usage
 // for multiple tests
 func TestMain(m *testing.M) {
-	sharedInfoSvc, _ = cloudinfo.NewCloudInfoServiceFromEnv("TF_VAR_ibmcloud_api_key", cloudinfo.CloudInfoServiceOptions{})
+	var err error
+	sharedInfoSvc, err = cloudinfo.NewCloudInfoServiceFromEnv("TF_VAR_ibmcloud_api_key", cloudinfo.CloudInfoServiceOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	os.Exit(m.Run())
 }
